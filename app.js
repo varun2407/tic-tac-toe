9d00503a165a94e513a97837fe8914332e18ab06
Varun Shembekar <varun247shembekar@gmail.com>
2023-10-18 17:47:19 -0400

    Add scores

--- app.js
+++ app.js
@@ -26,4 +26,37 @@ function addGo(e){
   go = go === "circle" ? "cross" : "circle"
   infoDisplay.textContent = "It is now " + go + "'s turn"
   e.target.removeEventListener("click", addGo)
+  checkScore()
+}
+
+function checkScore(){
+  const allSquares = document.querySelectorAll('.square')
+  const winningCombos=[
+    [0,1,2],[3,4,5],[6,7,8],
+    [0,3,6], [1,4,7],[2,5,8],
+    [0,4,8],[2,4,6]
+  ]
+
+  winningCombos.forEach(array => {
+    let circleWins = array.every(cell => 
+      allSquares[cell].firstChild?.classList.contains('circle'))
+      
+      if (circleWins){
+        infoDisplay.textContent = "Circle wins"
+        allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
+        return
+      }
+  })
+
+  winningCombos.forEach(array => {
+    let crossWins = array.every(cell => 
+      allSquares[cell].firstChild?.classList.contains('cross'))
+      
+      if (crossWins){
+        infoDisplay.textContent = "Cross wins"
+        allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
+        return
+      }
+  })
+  
 }
